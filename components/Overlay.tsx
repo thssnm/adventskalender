07b782a74400f3c2e4c4ade651a80dc8ebf0c679
thssn <theissen_matthias@hotmail.com>
2023-10-30 13:10:49 +0100

    design changes and add dummy data on number 2

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from "react";
+import { CalendarItemType } from "./numbers";
 
-export const Overlay = ({ item, visible, setVisible }: {
-    item: {
-        number: string;
-        title: string;
-        text: string;
-    }; visible: boolean; setVisible: (value: boolean) => void;
+export const Overlay = ({
+  item,
+  visible,
+  setVisible,
+}: {
+  item: CalendarItemType;
+  visible: boolean;
+  setVisible: (value: boolean) => void;
 }) => {
-    if (!visible) {
-        return null;
-    }
-    return <div onClick={() => setVisible(false)} style={{
+  const [showSpoiler, toggleShowSpoiler] = React.useState(false);
+  if (!visible) {
+    return null;
+  }
+  const time = new Date();
+  const hour = time.getHours();
+
+  const isOldItem = time.getDate() > Number(item.number);
+
+  const additionalInfos =
+    isOldItem || (hour >= 19 && time.getDay() === Number(item.number));
+
+  const spoilerText = additionalInfos
+    ? "Aufloesung"
+    : "Aufloesung erst ab 19 Uhr";
+
+  return (
+    <div
+      style={{
         backgroundColor: "rgba(0,0,0,0.8)",
         position: "absolute",
         top: 20,
@@ -19,15 +37,153 @@ export const Overlay = ({ item, visible, setVisible }: {
         bottom: 0,
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
-    }}
+        alignItems: "center",
+      }}
     >
-        <div style={{ width: "80%", height: "70%", maxHeight: 500, backgroundColor: "#828282cd", borderRadius: 12, padding: 24, overflowY: "scroll", borderColor: "white", borderWidth: 22 }}>
-            <h2 style={{ color: "white" }}>{item.number}</h2>
-            {item.text === "text" ?
-                <div style={{ textDecoration: "underline", cursor: "pointer" }} onClick={() => window.location.reload()}>Dieser Text ist noch nicht verfuegbar. Klicke hier um die Seite neu zu laden.</div>
-                : <div style={{ color: "white" }} dangerouslySetInnerHTML={{ __html: item.text }} />
-            }
+      <div
+        style={{
+          width: "80%",
+          maxWidth: 700,
+          height: "70%",
+          maxHeight: 500,
+          backgroundColor: "#828282cd",
+          borderRadius: 12,
+          padding: 24,
+          overflowY: "scroll",
+          borderColor: "white",
+          borderWidth: 22,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <h2 style={{ color: "white" }}>{item.number}</h2>
+          <div
+            onClick={() => setVisible(false)}
+            style={{
+              cursor: "pointer",
+              backgroundColor: "white",
+              borderRadius: 25,
+              width: 50,
+              height: 50,
+
+              textAlign: "center",
+            }}
+          >
+            <p style={{ color: "black" }}>X</p>
+          </div>
         </div>
-    </div>;
+
+        <div style={{ color: "white" }}>
+          {item.brewAdvices ? (
+            <p style={{ fontStyle: "italic" }}>Bruehempfehlung: </p>
+          ) : null}
+          {item.brewAdvices?.brewer ? <p>{item.brewAdvices?.brewer}</p> : null}
+          {item.brewAdvices?.grinding ? (
+            <p>{item.brewAdvices?.grinding}</p>
+          ) : null}
+          {item.brewAdvices?.temperature ? (
+            <p>{item.brewAdvices?.temperature}</p>
+          ) : null}
+          {item.brewAdvices?.ratio ? <p>{item.brewAdvices?.ratio}</p> : null}
+          {item.brewAdvices?.time ? <p>{item.brewAdvices?.time}</p> : null}
+          {item.brewAdvices?.notes ? (
+            <p>Anmerkungen: {item.brewAdvices?.notes}</p>
+          ) : null}
+        </div>
+        <div
+          style={{
+            borderRadius: 2,
+            borderColor: "white",
+            borderWidth: 2,
+            borderStyle: "solid",
+            backgroundColor: "#afadadcd",
+          }}
+        >
+          <div
+            onClick={() => toggleShowSpoiler(!showSpoiler)}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingRight: 8,
+            }}
+          >
+            <p
+              style={{
+                cursor: "pointer",
+                padding: 8,
+              }}
+            >
+              {showSpoiler ? "Zuklappen" : spoilerText}
+            </p>
+            <p>
+              {showSpoiler
+                ? String.fromCharCode(11014)
+                : String.fromCharCode(11015)}
+            </p>
+          </div>
+          {showSpoiler ? (
+            <div
+              style={{
+                backgroundColor: "#525252cd",
+                padding: 8,
+                width: "100%",
+              }}
+            >
+              {additionalInfos ? (
+                <div>
+                  {item.additionalInfos?.roastery ? (
+                    <p>Rösterei: {item.additionalInfos?.roastery}</p>
+                  ) : null}
+                  {item.additionalInfos?.name ? (
+                    <p>Name: {item.additionalInfos?.name}</p>
+                  ) : null}
+                  {item.additionalInfos?.city ? (
+                    <p>Stadt: {item.additionalInfos?.city}</p>
+                  ) : null}
+                  {item.additionalInfos?.taste ? (
+                    <p>Geschmack: {item.additionalInfos?.taste}</p>
+                  ) : null}
+                  {item.additionalInfos?.roast ? (
+                    <p>Röstung: {item.additionalInfos?.roast}</p>
+                  ) : null}
+                  {item.additionalInfos?.variety ? (
+                    <p>Varität: {item.additionalInfos?.variety}</p>
+                  ) : null}
+                  {item.additionalInfos?.region ? (
+                    <p>Region: {item.additionalInfos?.region}</p>
+                  ) : null}
+                  {item.additionalInfos?.processing ? (
+                    <p>Aufbereitung: {item.additionalInfos?.processing}</p>
+                  ) : null}
+                  {item.additionalInfos?.recommendation ? (
+                    <p>Empfohlen für: {item.additionalInfos?.recommendation}</p>
+                  ) : null}
+                  {item.additionalInfos?.url ? (
+                    <a
+                      target="_blank"
+                      href={item.additionalInfos?.url}
+                      rel="noopener noreferrer"
+                      style={{
+                        color: "lightblue",
+                        textDecoration: "underline",
+                      }}
+                    >
+                      Link
+                    </a>
+                  ) : null}
+                </div>
+              ) : (
+                <p>Die Aufloesung gibt es wie immer erst um 19 Uhr.</p>
+              )}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 };
--- components/numbers.ts
+++ components/numbers.ts
@@ -1,4 +1,32 @@
-export const numbers = [
+export type CalendarItemType = {
+  number: string;
+  title: string;
+  text: string;
+  brewAdvices: {
+    brewer: string;
+    grinding: string;
+    temperature: string;
+    ratio: string;
+    time: string;
+    notes: string;
+  };
+  additionalInfos: {
+    roastery: string;
+    name: string;
+    city: string;
+    taste: string;
+    roast: string;
+    variety: string;
+    region: string;
+    processing: string;
+    recommendation: string;
+    url: string;
+  };
+};
+
+export type Numbers = CalendarItemType[];
+
+export const numbers: Numbers = [
   { number: "10", title: "title", text: "" },
   { number: "12", title: "title", text: "" },
   {
@@ -11,7 +39,31 @@ export const numbers = [
   { number: "8", title: "title", text: "" },
   { number: "6", title: "title", text: "" },
   { number: "18", title: "title", text: "" },
-  { number: "2", title: "title", text: "" },
+  {
+    number: "2",
+    title: "title",
+    text: "",
+    brewAdvices: {
+      brewer: "Orea V3",
+      grinding: "Comandante 19 Clicks",
+      temperature: "91°C",
+      ratio: "15gr to 250gr",
+      time: "2:35min",
+      notes: "3 mal 50gr",
+    },
+    additionalInfos: {
+      roastery: "Cafe Mundus",
+      name: "Äthiopien – Amaderero",
+      city: "Dueren",
+      taste: "Rote Früchte, Schokokolade, floral",
+      roast: "Medium / Omniroast",
+      variety: "Heirloom",
+      region: "Yirgacheffe",
+      processing: "super natural",
+      recommendation: "Handfilter, Aeropress, Presskanne, Siebträger",
+      url: "https://www.cafemundus.com/products/athiopien-hambela?variant=46614488285508",
+    },
+  },
   { number: "23", title: "title", text: "" },
   { number: "13", title: "title", text: "" },
   { number: "21", title: "title", text: "" },
@@ -26,5 +78,38 @@ export const numbers = [
   { number: "16", title: "title", text: "" },
   { number: "22", title: "title", text: "" },
   { number: "11", title: "title", text: "" },
-  { number: "14", title: "title", text: "" },
+  {
+    number: "14",
+    title: "title",
+    text: "",
+  },
 ];
+
+/*   
+
+{
+    number: "",
+    title: "",
+    text: "",
+    brewAdvices: {
+      brewer: "",
+      grinding: "",
+      temperature: "",
+      ratio: "",
+      time: "",
+    },
+    additionalInfos: {
+      roastery: "",
+      name: "",
+      city: "",
+      taste: "",
+      roast: "",
+      variety: "",
+      region: "",
+      processing: "",
+      recommendation: "",
+      url: "",
+    },
+  }, 
+  
+  */
